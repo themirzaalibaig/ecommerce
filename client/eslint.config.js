eece303f41f8da443dd2cee67a21dac0854b506e
Ali Baig <baiga8424@gmail.com>
2025-10-03 16:28:18 +0500

    Add EditorConfig, Prettier configuration, and ESLint integration for consistent code formatting and linting; update package.json scripts for formatting checks and add necessary dependencies.

--- client/eslint.config.js
+++ client/eslint.config.js
@@ -4,6 +4,8 @@ import reactHooks from 'eslint-plugin-react-hooks'
 import reactRefresh from 'eslint-plugin-react-refresh'
 import tseslint from 'typescript-eslint'
 import { defineConfig, globalIgnores } from 'eslint/config'
+import prettierConfig from 'eslint-config-prettier'
+import prettierPlugin from 'eslint-plugin-prettier'
 
 export default defineConfig([
   globalIgnores(['dist']),
@@ -14,10 +16,17 @@ export default defineConfig([
       tseslint.configs.recommended,
       reactHooks.configs['recommended-latest'],
       reactRefresh.configs.vite,
+      prettierConfig,
     ],
+    plugins: {
+      prettier: prettierPlugin,
+    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
+    },
+    rules: {
+      'prettier/prettier': 'warn',
     },
   },
 ])
